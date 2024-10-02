1e294045d6023ab7fb690f208b9c8400b2052543
Rassel <rassel.billiono@student.umn.ac.id>
2024-10-02 09:59:47 +0700

    week6

--- App.js
+++ App.js
@@ -1,11 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import NIMInput from "./Input";
+
+const Input = ({ name, onChangeText }) => {
+  console.log(name);
+  return (
+    <View>
+      <Text>Name</Text>
+      <TextInput
+        placeholder="Input your name"
+        style={{
+          borderColor: "black",
+          borderWidth: 1,
+          padding: 10,
+          borderRadius: 8,
+        }}
+        onChangeText={onChangeText}
+      />
+    </View>
+  );
+};
 
 export default function App() {
+  const [name, setName] = useState("");
+  const [nim, setNim] = useState("");
+
+  const handleChangeMyName = (value) => {
+    setName(value);
+  };
+
+  const handleChangeMyNIM = (value) => {
+    setNim(value);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Rassel Billiono - 00000037399</Text>
-      <StatusBar style="auto" />
+      <Text>
+        {name} - {nim}
+      </Text>
+      <Input name={name} onChangeText={handleChangeMyName} />
+      <NIMInput nim={nim} onChangeText={handleChangeMyNIM} />
     </View>
   );
 }
@@ -13,8 +47,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
