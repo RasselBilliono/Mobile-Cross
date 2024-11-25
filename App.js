93da320d982e3f5f6bc77daf9387316fdfcf5df9
Rassel <rassel.billiono@student.umn.ac.id>
2024-11-25 23:52:04 +0700

    week10

--- App.js
+++ App.js
@@ -1,146 +1,104 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Button, PermissionsAndroid, Text, Platform } from "react-native";
-import { launchImageLibrary, launchCamera } from "react-native-image-picker";
-import RNFS from "react-native-fs"; 
+import {
+  StyleSheet,
+  View,
+  Button,
+  PermissionsAndroid,
+  Text,
+  Platform,
+} from "react-native";
+import Geolocation from '@react-native-community/geolocation';
 
-function App() {
-  const [uri, setUri] = useState("");
+const App = () => {
+  const [coords, setCoords] = useState(null);
 
-  const saveFile = async (photoUri) => {
-    try {
-      const fileName = "test_photo.jpg"; 
-      const path = RNFS.PicturesDirectoryPath + "/" + fileName;
+  const getLocation = async () => {
+    const hasPermission = await hasLocationPermission();
 
-      await RNFS.copyFile(photoUri, path);
-      console.log("Success: Photo saved to Pictures folder.");
-    } catch (err) {
-      console.error("Error saving photo:", err);
+    if (!hasPermission) {
+      return;
     }
-  };
 
-  const openImagePicker = async () => {
-    const granted = await requestGalleryPermission();
-    if (granted) {
-      launchImageLibrary(
-        {
-          mediaType: "photo",
-          includeBase64: false,
-          maxHeight: 2000,
-          maxWidth: 2000,
+    Geolocation.getCurrentPosition(
+      (position) => {
+        setCoords(position.coords);
+        console.log(position);
+      },
+      (error) => {
+        console.error(`Code ${error.code}`, error.message);
+        console.log(error);
+      },
+      {
+        accuracy: {
+          android: "high",
         },
-        handleResponse
-      );
-    }
-  };
-
-  const handleCameraLaunch = async () => {
-    const granted = await requestCameraPermission();
-    if (granted) {
-      launchCamera(
-        {
-          mediaType: "photo",
-          includeBase64: false,
-          maxHeight: 2000,
-          maxWidth: 2000,
-        },
-        handleResponse
-      );
-    }
-  };
-
-  const requestCameraPermission = async () => {
-    try {
-      const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.CAMERA,
-        {
-          title: "Camera Permission",
-          message: "This app needs access to your camera to take photos.",
-          buttonNeutral: "Ask Me Later",
-          buttonNegative: "Cancel",
-          buttonPositive: "OK",
-        }
-      );
-      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-        console.log("Camera permission granted");
-        return true;
-      } else {
-        console.log("Camera permission denied");
-        return false;
+        enableHighAccuracy: true,
+        timeout: 15000,
+        distanceFilter: 0,
+        forceRequestLocation: true,
+        showLocationDialog: true,
       }
-    } catch (err) {
-      console.warn(err);
-      return false;
-    }
+    );
   };
 
-  const requestGalleryPermission = async () => {
-    try {
-      if (Platform.OS === "android" && Platform.Version >= 29) {
-        const granted = await PermissionsAndroid.request(
-          PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
-          {
-            title: "Gallery Permission",
-            message: "This app needs access to your gallery to select photos.",
-            buttonNeutral: "Ask Me Later",
-            buttonNegative: "Cancel",
-            buttonPositive: "OK",
-          }
-        );
-        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-          console.log("Gallery permission granted");
-          return true;
-        } else {
-          console.log("Gallery permission denied");
-          return false;
-        }
-      } else {
-        const granted = await PermissionsAndroid.request(
-          PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
-          {
-            title: "Storage Permission",
-            message: "This app needs access to your storage to select photos.",
-            buttonNeutral: "Ask Me Later",
-            buttonNegative: "Cancel",
-            buttonPositive: "OK",
-          }
-        );
-        return granted === PermissionsAndroid.RESULTS.GRANTED;
-      }
-    } catch (err) {
-      console.warn(err);
-      return false;
+  const hasLocationPermission = async () => {
+    if (Platform.OS === "android" && Platform.Version < 23) {
+      return true;
     }
-  };
 
-  const handleResponse = (response) => {
-    if (response.didCancel) {
-      console.log("User cancelled image picker");
-    } else if (response.error) {
-      console.log("Image picker error: ", response.error);
-    } else if (response.assets && response.assets.length > 0) {
-      const imageUri = response.assets[0].uri;
-      setUri(imageUri);
-      saveFile(imageUri); 
-    } else {
-      console.log("No assets found in the response");
+    const hasPermission = await PermissionsAndroid.check(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+    );
+
+    if (hasPermission) {
+      return true;
     }
+
+    const status = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+    );
+
+    if (status === PermissionsAndroid.RESULTS.GRANTED) {
+      return true;
+    }
+
+    if (status === PermissionsAndroid.RESULTS.DENIED) {
+      console.log("Location permission denied by user.");
+    } else if (status === PermissionsAndroid.RESULTS.NEVER_ASK_AGAIN) {
+      console.log("Location permission denied by user.");
+    }
+
+    return false;
   };
 
   return (
     <View style={styles.container}>
-      <Button title="Open Image Picker" onPress={openImagePicker} />
-      <Button title="Launch Camera" onPress={handleCameraLaunch} />
-      {uri ? <Text>Selected Image URI: {uri}</Text> : <Text>No image selected</Text>}
+      <Text style={styles.title}>Get Geo Location</Text>
+      <Button title="Get Geo Location" onPress={getLocation} />
+      {coords && (
+        <View style={styles.locationContainer}>
+          <Text>Longitude: {coords.longitude}</Text>
+          <Text>Latitude: {coords.latitude}</Text>
+        </View>
+      )}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    padding: 16,
+    padding: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  locationContainer: {
+    marginTop: 20,
   },
 });
 
--- services/axios.ts
+++ services/axios.ts
@@ -1,18 +1,3 @@
 import axios from "axios";
 
 const ENV = process.env.EXPO_PUBLIC_API_URL;
-
-export const getUsers = () => {
-    return axios.get(ENV + "users");
-    
-};
-
-export const getPosts = () => {
-    return axios.get(ENV + "posts");
-    
-};
-
-export const updatePost = (id, data) => {
-    return axios.put(`${ENV}posts/${id}`, data);
-  };
-  
